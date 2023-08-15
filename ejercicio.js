a72e5b2618ab8d5aa773048a1ebb687c41bcd3a8
marianita13 <marianitacero.1913@gmail.com>
2023-08-15 07:33:45 -0500

    descuentos

--- ejercicio.js
+++ ejercicio.js
@@ -12,14 +12,31 @@
 function arreglo(numeros, suma) {
     let numbers = [];
     for (i = 0; i < numeros.length; i++) {
-        for (p = 1; p < numeros.length; p++) { 
+        for (p = i+1; p < numeros.length; p++) { 
             if (numeros[i] + numeros[p] == suma) { 
-                parejas.push([numeros[i], numeros[p]]);
+                numbers.push([numeros[i], numeros[p]]);
             }
         }
     }
-    
     return numbers;
 }
 
 console.log(arreglo([2, 4, 3, 6, 9, 8], 10));
+
+// Hay un artículo en promoción, si el cliente está entre los 20 primeros clientes, tiene 30%
+// de descuento y entre los primeros 50 clientes, un 10% de descuento. Haga una
+// función que permita saber cuánto descuento tiene el cliente, según el orden en que llegó.
+
+function orden(numero){
+    if (numero>=1 && numero<=20){
+        descuento = console.log("Obtienes un descuento del 20%");
+    }
+    else if (numero>20 && numero<=50){
+        descuento = console.log("Obtienes un descuento del 10%");
+    }
+    else{
+        descuento = console.log("Obtienes un descuento del 0%");
+    }
+    return descuento
+}
+orden(90)
