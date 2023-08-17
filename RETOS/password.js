14e8c17755c3c7d46a5f7ee07a0b8d7d6094d247
Mariana Acero <124885480+marianita13@users.noreply.github.com>
2023-08-17 09:22:35 -0500

    Actualizar password.js
    Finalizacion password

--- RETOS/password.js
+++ RETOS/password.js
@@ -1,24 +1,48 @@
 const readline = require('readline');
 
-const pregunta = readline.createInterface({
+const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+const longitud_minima = 1;
+const longitud_maxima = 25000;
 
-newPassword = ""
+let newPassword = '';
 
-const preguntas = () => {
-    rl.question("Ingrese la primera contraseña: ", (pass1) => {
-        const isLower1 = (str) => pass1 === str.toLowerCase();
-        if (isLower1 == true){
-            rl.question("Ingrese la segunda contraseña: ", (pass2) => {
-                const isLower2 = (str) => pass2 === str.toLowerCase();
-                
-            })
-        }else{
-            console.log("La primera contraseña contiene mayusculas")
-            preguntas()
-        }
-        
-    })
-}
+// Para verificar si una cadena es minúscula
+const minuscula = (str) => /^[a-z]+$/.test(str) && /\d/.test(str);
+
+
+// Acabar la función y mostrar la nueva contraseña
+const finalizar = () => {
+    rl.close();
+    console.log(`Nueva contraseña generada: ${newPassword}`);
+};
+
+// Preguntas de las contraseñas
+const passwords = () => {
+    rl.question("Ingrese la primera contraseña: ", (password1) => {
+        rl.question("Ingrese la segunda contraseña: ", (password2) => {
+            if (password1.length !== password2.length) {
+                console.clear()
+                console.log("Las contraseñas no tienen la misma longitud");
+                passwords(); // Volver a preguntar
+            } else if ((password1.length < longitud_minima || password1.length > longitud_maxima) || (password2.length < longitud_minima || password2.length > longitud_maxima)) {
+                console.clear()
+                console.log("La contraseña debe tener más caracteres que 1 y menos que 25000");
+                passwords(); // Volver a preguntar
+            } else if (!minuscula(password1) || !minuscula(password2)) {
+                console.clear()
+                console.log('Las contraseñas solo deben tener letras minúsculas.');
+                passwords(); // Volver a preguntar
+            } else {
+                for (let i = 0; i < password1.length; i++) {
+                    newPassword += password1[i] + password2[i];
+                }
+                finalizar();
+            }
+        });
+    });
+};
+console.clear()
+passwords();
